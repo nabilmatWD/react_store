003dc004f73b055cb68c2e750ea9433cd3ae663d
nabil <nabilmat@gmail.com>
2019-12-15 09:46:30 +0000

    second commit

---
+++ src/components/Hero.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+import mainBcg from "../images/mainBcg.jpeg";
+
+export default function Hero({ img, title, max, children }) {
+  return (
+    <HeroWrapper max={max} img={img}>
+      <div className="banner">
+        <h1 className="title">{title}</h1>
+        {children}
+      </div>
+    </HeroWrapper>
+  );
+}
+
+const HeroWrapper = styled.div`
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: ${props => (props.max ? "100vh" : "60vh")};
+  color: var(--mainWhite);
+  background: linear-gradient(var(--primaryRGBA), var(--primaryRGBA)),
+    url(${props => props.img}) center/ cover no-repeat;
+  .title {
+    padding-top: 2rem;
+    font-size: 3.5rem;
+  }
+  .subtitle {
+    color: #ec9b04;
+    background-color: #000;
+    padding: 0.5rem;
+    margin-bottom: 4rem;
+  }
+`;
+
+Hero.defaultProps = {
+  img: mainBcg
+};
